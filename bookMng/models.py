af42ec4ca96282deaee07152173d1aab3a312b71
luci4k <aldana.luciano@yahoo.com>
2025-04-27 13:29:00 -0700

    Added New Model to support Navbar Icons

--- bookMng/models.py
+++ bookMng/models.py
@@ -10,6 +10,13 @@ class MainMenu(models.Model):
     item = models.CharField(max_length=300, unique=True)
     link = models.CharField(max_length=300, unique=True)
 
+    # New model for icons
+    icon_class = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+
     def __str__(self):
         return self.item
 
