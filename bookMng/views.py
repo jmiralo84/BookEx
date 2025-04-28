8ab1e69ce048146332aecbab18157287eddae187
luci4k <aldana.luciano@yahoo.com>
2025-04-27 18:00:00 -0700

    Improved General UI, added Breadcrumb on top navbar and moved login to the left navbar

--- bookMng/views.py
+++ bookMng/views.py
@@ -16,18 +16,28 @@ from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.urls import reverse
 
 
 def index(request):
+    breadcrumb_list = [
+        {'name': 'Home', 'url': None}  # Home is the current page
+    ]
+
     return render(request,
                   'bookMng/index.html',
                   {
                       # 'item_list': MainMenu.objects.all()
+                      'breadcrumb_list': breadcrumb_list,
                   })
 
 
 def postbook(request):
     submitted = False
+    breadcrumb_list = [
+        {'name': 'Home', 'url': reverse('index')},  # Link back to home
+        {'name': 'Post Book', 'url': None}  # Current page
+    ]
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
@@ -48,30 +58,41 @@ def postbook(request):
                   {
                       'form': form,
                       # 'item_list': MainMenu.objects.all(),
-                      'submitted': submitted
+                      'submitted': submitted,
+                      'breadcrumb_list': breadcrumb_list,
                   })
 
 def displaybooks(request):
     books = Book.objects.all()
+    breadcrumb_list = [
+        {'name': 'Home', 'url': reverse('index')},
+        {'name': 'Display Books', 'url': None}
+    ]
     for b in books:
         b.pic_path = b.picture.url[14:]
     return render(request,
                   'bookMng/displaybooks.html',
                   {
                       # 'item_list': MainMenu.objects.all(),
-                      'books': books
+                      'books': books,
+                      'breadcrumb_list': breadcrumb_list,
                   })
 
 
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
-
+    breadcrumb_list = [
+        {'name': 'Home', 'url': reverse('index')},
+        {'name': 'Display Books', 'url': reverse('displaybooks')},  # Link back to list
+        {'name': book.name, 'url': None}  # Current book, use its name
+    ]
     book.pic_path = book.picture.url[14:]
     return render(request,
                   'bookMng/book_detail.html',
                   {
                       # 'item_list': MainMenu.objects.all(),
-                      'book': book
+                      'book': book,
+                      'breadcrumb_list': breadcrumb_list,
                   })
 
 def book_delete(request, book_id):
@@ -95,6 +116,10 @@ class Register(CreateView):
 
 
 def mybooks(request):
+    breadcrumb_list = [
+        {'name': 'Home', 'url': reverse('index')},
+        {'name': 'My Books', 'url': None}
+    ]
     books = Book.objects.filter(username=request.user)
     for b in books:
         b.pic_path = b.picture.url[14:]
@@ -102,7 +127,8 @@ def mybooks(request):
                   'bookMng/mybooks.html',
                   {
                       # 'item_list': MainMenu.objects.all(),
-                      'books': books
+                      'books': books,
+                      'breadcrumb_list': breadcrumb_list,
                   })
 
 def add_to_cart(request, book_id):
@@ -133,9 +159,14 @@ def my_cart(request):
                   {
                       # "item_list": MainMenu.objects.all(),
                       "books": books,
+
                   })
 
 def cart_page(request):
+    breadcrumb_list = [
+        {'name': 'Home', 'url': reverse('index')},
+        {'name': 'Cart', 'url': None}
+    ]
     cart_ids = request.session.get("cart", [])
     books = Book.objects.filter(id__in=cart_ids)
 
@@ -148,6 +179,7 @@ def cart_page(request):
             # "item_list": MainMenu.objects.all(),
             "books": books,
             "subtotal": subtotal,
+            'breadcrumb_list': breadcrumb_list,
         },
     )
 def remove_from_cart(request, book_id):
